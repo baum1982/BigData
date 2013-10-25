95746cb4f2673798a1f9150f8ae11fc937f1eb79
baum1982 <baum1982@gmail.com>
2013-10-25 17:32:30 +0900

    .

---
+++ MapRduce/src/org/baum/hadoop/chapter05/DepartureDelayCountMapper.java
@@ -0,0 +1,49 @@
+package org.baum.hadoop.chapter05;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class DepartureDelayCountMapper extends
+		Mapper<LongWritable, Text, Text, IntWritable> {
+		
+	// map 출력값
+	private final static IntWritable outputValue = new IntWritable(1);
+	
+	// map 출력기
+	private Text outputKey = new Text();
+	
+	
+	@Override
+	protected void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		
+		if(key.get() > 0){
+			// 콤마 구분자 분리
+			String[] columns = value.toString().split(",");
+			
+			if(columns != null && columns.length > 0){
+				
+				// 헤더는 무시
+				if("Year".equals(columns[0]))
+					return;
+				
+				
+				// 출력키 설정 (년,월)
+				outputKey.set(columns[0] + "," + columns[1]);
+				
+				// [15]: DepDelay, 출발지연
+				if(!"NA".equals(columns[15])){
+					int depDelayTime = 	Integer.parseInt(columns[15]);
+					if(depDelayTime > 0){
+						// 출력 데이터 생성
+						context.write(outputKey, outputValue);
+					}
+				}
+			}
+		}
+	}
+}
